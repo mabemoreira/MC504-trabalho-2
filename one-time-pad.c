705892ca55f5877055e525a0ec05337dc6be91cf
viniguedes29 <f260440@dac.unicamp.br>
2025-05-25 17:43:19 -0300

    fix: erros syscall signature

--- one-time-pad.c
+++ one-time-pad.c
@@ -3,46 +3,7 @@
 #include <string.h>
 #include <linux/syscalls.h>
 
-
-void get_mensagem_original(char *cifrada, char *chave, char *mensagem_decifrada, unsigned long tamanho_cifrada, unsigned long tamanho_chave) {
-    if (tamanho_chave < tamanho_cifrada) {
-        fprintf(stderr, "Erro: chave menor que a mensagem.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    void *kcifrada;
-    kcifrada = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kcifrada)
-        return -ENOMEM;
-    if (copy_from_user(kcifrada, cifrada, tamanho_cifrada))
-        return -EFAULT;
-
-    void *kchave;
-    kchave = kmalloc(tamanho_chave GFP_KERNEL);
-    if (!kchave)
-        return -ENOMEM;
-    if (copy_from_user(kchave, chave, tamanho_cifrada))
-        return -EFAULT;
-    
-    void *kmensagem;
-    kmensagem = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kmensagem)
-        return -ENOMEM;
-
-    
-    for (size_t i = 0; i < tamanho_cifrada; i++) {
-        kmensagem[i] = kcifrada[i] ^ kchave[i];
-    }
-    kmensagem[tamanho_cifrada] = '\0';
-
-    if(copy_to_user(mensagem_decifrada, kmensagem, tamanho_cifrada))
-        return -EFAULT;
-    kfree(kcifrada);
-    kfree(kchave);
-    kfree(kmensagem);
-}
-
-SYSCALL_DEFINE5(get_mensagem_original,  void __user *, mensagem,  void __user *, chave,  void __user *, 
+SYSCALL_DEFINE5(get_mensagem_original,  void __user * mensagem,  void __user * chave,  void __user * 
                 cifrada, unsigned long tamanho_cifrada,  unsigned long tamanho_chave){
 
     if (tamanho_chave < tamanho_cifrada) {
@@ -77,6 +38,7 @@ SYSCALL_DEFINE5(get_mensagem_original,  void __user *, mensagem,  void __user *,
 
     if(copy_to_user(mensagem_decifrada, kmensagem, tamanho_cifrada))
         return -EFAULT;
+
     kfree(kcifrada);
     kfree(kchave);
     kfree(kmensagem);
@@ -84,8 +46,8 @@ SYSCALL_DEFINE5(get_mensagem_original,  void __user *, mensagem,  void __user *,
 
 }
 
-SYSCALL_DEFINE5(set_mensagem_cifrada, void __user *, mensagem, void __user *, chave, 
-    void __user *, cifrada, unsigned long tamanho_cifrada, unsigned long tamanho_chave) {
+SYSCALL_DEFINE5(set_mensagem_cifrada, void __user * mensagem, void __user * chave, 
+    void __user * cifrada, unsigned long tamanho_cifrada, unsigned long tamanho_chave) {
     
     if (tamanho_chave < tamanho_cifrada) {
     fprintf(stderr, "Erro: chave menor que a mensagem.\n");
@@ -130,43 +92,6 @@ SYSCALL_DEFINE5(set_mensagem_cifrada, void __user *, mensagem, void __user *, ch
 
 }
 
-void get_mensagem_original(char *cifrada, char *chave, char *mensagem_decifrada, unsigned long tamanho_cifrada, unsigned long tamanho_chave) {
-    if (tamanho_chave < tamanho_cifrada) {
-        fprintf(stderr, "Erro: chave menor que a mensagem.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    void *kcifrada;
-    kcifrada = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kcifrada)
-        return -ENOMEM;
-    if (copy_from_user(kcifrada, cifrada, tamanho_cifrada))
-        return -EFAULT;
-
-    void *kchave;
-    kchave = kmalloc(tamanho_chave GFP_KERNEL);
-    if (!kchave)
-        return -ENOMEM;
-    if (copy_from_user(kchave, chave, tamanho_cifrada))
-        return -EFAULT;
-    
-    void *kmensagem;
-    kmensagem = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kmensagem)
-        return -ENOMEM;
-
-    
-    for (size_t i = 0; i < tamanho_cifrada; i++) {
-        kmensagem[i] = kcifrada[i] ^ kchave[i];
-    }
-    kmensagem[tamanho_cifrada] = '\0';
-
-    if(copy_to_user(mensagem_decifrada, kmensagem, tamanho_cifrada))
-        return -EFAULT;
-    kfree(kcifrada);
-    kfree(kchave);
-    kfree(kmensagem);
-}
 int main() {
     char mensagem[256] = "ola mc504";
     char chave[256] =    "aaaaaaaaa";
