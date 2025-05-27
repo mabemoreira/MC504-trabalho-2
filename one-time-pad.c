9970bd0d2835bc4a037a5626479574f8d6920c6a
Maria Beatriz Moreira <mariabeatrizmoreira20@gmail.com>
2025-05-27 15:37:38 -0300

    updated

--- one-time-pad.c
+++ one-time-pad.c
@@ -1,115 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <linux/syscalls.h>
 
-SYSCALL_DEFINE5(get_mensagem_original, unsigned char __user * mensagem,  unsigned char __user * chave,  unsigned char __user * 
-                cifrada, unsigned long tamanho_cifrada,  unsigned long tamanho_chave){
-
-    if (tamanho_chave < tamanho_cifrada) {
+void set_mensagem_cifrada(char *mensagem,  char *chave, char *cifrada, size_t tamanho_mensagem, size_t tamanho_chave) {
+    if (tamanho_chave < tamanho_mensagem) {
         fprintf(stderr, "Erro: chave menor que a mensagem.\n");
         exit(EXIT_FAILURE);
     }
-
-    void *kcifrada;
-    kcifrada = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kcifrada)
-        return -ENOMEM;
-    if (copy_from_user(kcifrada, cifrada, tamanho_cifrada))
-        return -EFAULT;
-
-    void *kchave;
-    kchave = kmalloc(tamanho_chave GFP_KERNEL);
-    if (!kchave)
-        return -ENOMEM;
-    if (copy_from_user(kchave, chave, tamanho_cifrada))
-        return -EFAULT;
-    
-    void *kmensagem;
-    kmensagem = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kmensagem)
-        return -ENOMEM;
-
-    
-    for (size_t i = 0; i < tamanho_cifrada; i++) {
-        kmensagem[i] = kcifrada[i] ^ kchave[i];
+    for (size_t i = 0; i < tamanho_mensagem; i++) {
+        cifrada[i] = mensagem[i] ^ chave[i];
+        mensagem[i] = '#';
     }
-    kmensagem[tamanho_cifrada] = '\0';
-
-    if(copy_to_user(mensagem_decifrada, kmensagem, tamanho_cifrada))
-        return -EFAULT;
-
-    kfree(kcifrada);
-    kfree(kchave);
-    kfree(kmensagem);
-
-
 }
 
-SYSCALL_DEFINE5(set_mensagem_cifrada, unsigned char __user * mensagem, unsigned char __user * chave, 
-    unsigned char __user * cifrada, unsigned long tamanho_cifrada, unsigned long tamanho_chave) {
-
-    if (tamanho_chave < tamanho_cifrada) {
-    fprintf(stderr, "Erro: chave menor que a mensagem.\n");
-    exit(EXIT_FAILURE);
+void get_mensagem_original(char *cifrada, char *chave, char *mensagem, size_t tamanho_mensagem, size_t tamanho_chave) {
+    if (tamanho_chave < tamanho_mensagem) {
+        fprintf(stderr, "Erro: chave menor que a mensagem.\n");
+        exit(EXIT_FAILURE);
     }
-
-
-    void *kmensagem;
-    *kmensagem = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kmensagem)
-        return -ENOMEM;
-    if (copy_from_user(kmensagem, mensagem, tamanho_cifrada))
-        return -EFAULT;
-
-    void *kchave;
-    kchave = kmalloc(tamanho_chave, GFP_KERNEL);
-    if (!kchave)
-        return -ENOMEM;
-    if (copy_from_user(kchave, chave, tamanho_chave))
-        return -EFAULT;
-
-    void *kcifrada;
-    kcifrada = kmalloc(tamanho_cifrada, GFP_KERNEL);
-    if (!kcifrada)
-        return -ENOMEM;
-    if (copy_from_user(kcifrada, cifrada, tamanho_cifrada))
-        return -EFAULT;
-
-
-    for (unsigned long i = 0; i < tamanho_cifrada; i++) {
-        kcifrada[i] = kmensagem[i] ^ kchave[i];
+    for (size_t i = 0; i < tamanho_mensagem; i++) {
+        mensagem[i] = cifrada[i] ^ chave[i];
     }
-
-    if(copy_to_user(cifrada, kcifrada, tamanho_cifrada))
-        return -EFAULT;
-
-
-
-    kfree(kmensagem);
-    kfree(kchave);
-    kfree(kcifrada);
-
+    mensagem[tamanho_mensagem] = '\0';
 }
 
 int main() {
-    char mensagem[256] = "ola mc504";
-    char chave[256] =    "aaaaaaaaa";
-    char cifrada[256], decifrada[256];
-
-    
-    set_mensagem_cifrada(mensagem, chave, cifrada, 9, 9);
-
-    printf("Cifrada: ");
-    for (size_t i = 0; i < 9; i++) {
-        printf("%d", cifrada[i]); 
+    char mensagem[256] = "Oie do contra";
+    char copia[256] = "Oie do contra";
+    char chave[256] = "SenhaSecretaTesteMensagem";
+    unsigned char cifrada[256], decifrada[256];
+   
+    set_mensagem_cifrada(mensagem, chave, cifrada, strlen(mensagem), strlen(chave));
+    printf("Mensagem cifrada: ");
+    for (size_t i = 0; i < strlen(mensagem); i++) {
+        printf("%02x ", cifrada[i]);
     }
     printf("\n");
-
-
-    get_mensagem_original(cifrada, chave, decifrada, 9, 9);
-
-    printf("Decifrada: %s\n", decifrada);
+    get_mensagem_original(cifrada, chave, decifrada, strlen(mensagem), strlen(chave));
+    printf("Mensagem decifrada: %s\n", decifrada);
+    if (strcmp(copia, decifrada) != 0) {
+        fprintf(stderr, "Erro: a mensagem decifrada não corresponde à mensagem original.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("A mensagem decifrada corresponde à mensagem original.\n");
 
     return 0;
 }
